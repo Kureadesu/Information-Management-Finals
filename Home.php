a77a9169adc786c9efaa7ba4963648a8d96ef023
Kureadesu <balaba.claire2003@gmail.com>
2024-11-24 17:28:47 +0800

    commit

--- Home.php
+++ Home.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
@@ -11,13 +11,13 @@
         <h1>Logistics Company</h1>
         <nav>
             <ul>
-                <li><a href="services">Services</a><li>
-                <li><a href="tracking">Tracking</a><li>
-                <li><a href="contact">Contact Us</a><li>
+                <li><a href="#services">Services</a><li>
+                <li><a href="#tracking">Tracking</a><li>
+                <li><a href="#contact">Contact Us</a><li>
             </ul>
         </nav>
     </header>
-
+    Document
     <main>
         <section id="services">
             <h2>Our Services</h2>
@@ -25,12 +25,33 @@
                 <h3>Transport</h3>
                 <p>ikaw nlng mag lagay ng pwedi dito lagay HAHAHAHA palitan nalng HAHA</p>
             </div>
-        <div class="services">
-            <h2>Our Services</h2>
-            <div cl>ass="servies">
-                <h3>Transport</h3>
-                <p>ikaw nlng mag lagay ng pwedi dito lagay HAHAHAHA palitan nalng HAHA</p>
-            </div>
+        <div class="service">
+            <h3>Warehousing</h3>
+            <p>ikaw nlng mag lagay ng pwedi dito lagay HAHAHAHA palitan nalng HAHA</p>
+        </div>
+        <div class="service">
+            <h3>Customs Clearance</h3>
+            <p>ikaw nlng mag lagay ng pwedi dito lagay HAHAHAHA palitan nalng HAHA</p>
+        </div>
+        <div class="service">
+            <h3>Customs Clearance</h3>
+            <p>ikaw nlng mag lagay ng pwedi dito lagay HAHAHAHA palitan nalng HAHA</p>
+        </div>
+        </section>
+
+        <section id="tracking">
+            <h2>Track Your Shipment</h2>
+            <form>
+            <label for="tracking-number">Enter your tracking number:</label>
+                <input type="text" id="tracking-number" name="tracking-number" required>
+                <button type="submit">Track</button>
+            </form>
+        </section>
     </main>
+    <footer>
+        <h2>Contact</h2>
+        <p>Email: Philsca</p>
+        <p>Phone: 63+ 911</p>
+    </footer>
 </body>
 </html>
