ac7b41fc0997529d9c018e563cfc3c894d9bd153
Kureadesu <balaba.claire2003@gmail.com>
2024-11-24 14:01:45 +0800

    committed

--- Home.php
+++ Home.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
@@ -15,6 +16,21 @@
                 <li><a href="contact">Contact Us</a><li>
             </ul>
         </nav>
-            
+    </header>
+
+    <main>
+        <section id="services">
+            <h2>Our Services</h2>
+            <div cl>ass="servies">
+                <h3>Transport</h3>
+                <p>ikaw nlng mag lagay ng pwedi dito lagay HAHAHAHA palitan nalng HAHA</p>
+            </div>
+        <div class="services">
+            <h2>Our Services</h2>
+            <div cl>ass="servies">
+                <h3>Transport</h3>
+                <p>ikaw nlng mag lagay ng pwedi dito lagay HAHAHAHA palitan nalng HAHA</p>
+            </div>
+    </main>
 </body>
 </html>
